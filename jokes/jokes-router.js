452790bb0c2e707daed10de90758d5f13b990f10
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-05-22 12:22:29 -0400

    Endpoints and all that is needed written. Now to do testing. Yay.

--- jokes/jokes-router.js
+++ jokes/jokes-router.js
@@ -1,19 +1,24 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
+// use restricted middleware to access jokes
+const restricted = require("../auth/authenticate-middleware");
+
+router.use(restricted);
+
+router.get("/", (req, res) => {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" },
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
-    .then(response => {
+    .get("https://icanhazdadjoke.com/search", requestOptions)
+    .then((response) => {
       res.status(200).json(response.data.results);
     })
-    .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+    .catch((err) => {
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 });
 
